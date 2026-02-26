618f9a706bd1b43df44dc78a6fff9a01959a9464
joelclemence <jclemence@companieshouse.gov.uk>
2026-02-26 09:54:22 +0000

    Create shell Spring Boot application

---
+++ src/main/java/uk/gov/companieshouse/developer/interview/DeveloperInterviewJavaApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.developer.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperInterviewJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperInterviewJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/developer/interview/DeveloperInterviewJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.developer.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperInterviewJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
